d908091bc0ab5be74dd78b7b508e882a71be00e2
berglindoma13 <berglindoma13@ru.is>
2017-01-23 12:12:24 +0000

    first commit

---
+++ js/canvasproject.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Berglind on 23.1.2017.
+ */
+
+$(document).ready(function(){
+    var canvas = document.getElementById("MyCanvas1");
+    var context = canvas.getContext("2d");
+
+    $("#MyCanvas1").mousedown(function(e){
+        console.log("here");
+        var xCoord = e.pageX - this.offsetLeft;
+        var yCoord = e.pageY - this.offsetTop;
+
+        //Rectangle drawing
+        context.fillStyle = "black";
+        context.strokeRect(xCoord,yCoord,10,10);
+
+        //line drawing
+        context.beginPath();
+        context.moveTo(xCoord,yCoord);
+        context.lineTo(xCoord+10, yCoord+10);
+        context.stroke();
+
+        
+
+    });
+});
+
