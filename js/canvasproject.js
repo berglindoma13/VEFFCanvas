1a52c0a68078074e7f6ee8175d4c485a76043ff2
berglindoma13 <berglindoma13@ru.is>
2017-01-24 09:51:19 +0000

    classes added

--- js/canvasproject.js
+++ js/canvasproject.js
@@ -3,6 +3,14 @@
  */
 
 //To save, use json object. There is an api to call when saving or loading data
+class Shape{
+    constructor(x,y,color){
+        this.xCoord = x;
+        this.yCoord = y;
+        this.selectedColor = color;
+    }
+}
+
 
 class Point{
     constructor(x,y){
@@ -12,39 +20,88 @@ class Point{
 
 }
 
-class Rectangle{
-    constructor(x1,y1,x2,y2){
-        this.topLeft = new Point(x1,y1);
+class Rectangle extends Shape{
+    constructor(x1,y1,x2,y2,color){
+        super(x1,y1,color);
         this.topRight = new Point(x2,y1);
         this.bottomLeft = new Point(x1,y2);
         this.bottomRight = new Point(x2,y2);
     }
 
     draw(context){
-        context.strokeRect(this.bottomLeft.xCoord,this.topLeft.yCoord,this.topRight.xCoord - this.topLeft.xCoord,this.bottomRight.yCoord - this.topLeft.yCoord);
+        context.strokeRect(this.bottomLeft.xCoord,this.yCoord,this.topRight.xCoord - this.xCoord,this.bottomRight.yCoord - this.yCoord);
 
     }
 }
 
+class Pen extends Shape{
+    constructor(x,y,color){
+        super(x,y,color);
+    }
+
+    //line drawing
+    //draw for each two points in the points array for
+    /*context.beginPath();
+     context.moveTo(current.xCoord,current.yCoord);
+     context.lineTo(current.xCoord+10, current.yCoord+10);
+     context.stroke();*/
+}
+
+class Line extends Shape{
+    constructor(x,y,color){
+        super(x,y,color);
+    }
+    //line drawing
+    /*context.beginPath();
+     context.moveTo(current.xCoord,current.yCoord);
+     context.lineTo(current.xCoord+10, current.yCoord+10);
+     context.stroke();*/
+}
+
+class Circle extends Shape{
+    constructor(x,y,color){
+        super(x,y,color);
+    }
+
+    //circle drawing
+    /*context.beginPath();
+     context.arc(current.xCoord,current.yCoord,10,0,2*Math.PI,false);
+     context.strokeStyle = "black";
+     context.stroke();*/
+}
+
+class Text{
+    //Attributes: Font, FontSize
+    //text drawing
+    /*context.strokeText("Text",current.xCoord,current.yCoord,10);
+     //text calculating how much space a text needs
+     context.measureText("Text");*/
+}
+
+
 var objectArray = [];
 
 $(document).ready(function(){
-    var canvas = document.getElementById("MyCanvas1");
-    var context = canvas.getContext("2d");
 
-    //default settings
-    var nextObject = "Pen";
-    var nextColor = "Black";
+    var settings = {
+        canvas : document.getElementById("MyCanvas1"),
+        nextObject : "Pen",
+        nextColor : "Black",
+        isDrawing : false
+
+    };
+
+    var context = settings.canvas.getContext("2d");
 
     var beginPoint;
-    var isDrawing = false;
+
 
     $("#MyCanvas1").mousedown(function(e){
         var xCoord = e.pageX - this.offsetLeft;
         var yCoord = e.pageY - this.offsetTop;
 
         beginPoint = new Point(xCoord,yCoord);
-        isDrawing = true;
+        settings.isDrawing = true;
 
     });
 
@@ -58,40 +115,28 @@ $(document).ready(function(){
 
         if( isDrawing === true){
             //Rectangle drawing
-            context.fillStyle = nextColor;
+            context.fillStyle = settings.nextColor;
             var tmpRect = new Rectangle(beginPoint.xCoord,beginPoint.yCoord,currentEnd.xCoord,currentEnd.yCoord);
             objectArray.push(tmpRect);
             tmpRect.draw(context);  
         }
 
-
-        //line drawing
-        /*context.beginPath();
-        context.moveTo(current.xCoord,current.yCoord);
-        context.lineTo(current.xCoord+10, current.yCoord+10);
-        context.stroke();*/
-
-        //circle drawing
-        /*context.beginPath();
-        context.arc(current.xCoord,current.yCoord,10,0,2*Math.PI,false);
-        context.strokeStyle = "black";
-        context.stroke();*/
-
-        //text drawing
-        /*context.strokeText("Text",current.xCoord,current.yCoord,10);
-        //text calculating how much space a text needs
-        context.measureText("Text");*/
     });
 
     $("#MyCanvas1").mouseup(function(e){
-        isDrawing = false;
-        for(var i = 0; i < objectArray.length; i++){
-            console.log(objectArray[i]);
-        }
-        var Image = canvas.toDataURL();
+        settings.isDrawing = false;
+
+        var Image = settings.canvas.toDataURL();
         document.getElementById('canvasImg').src = Image;
 
         //if (nextObject == "Rectangle"){}
     });
+
+    function drawCompleteCanvas(){
+        context.clearRect(0,0,500,500);
+        for(var i = 0; i < objectArray.length; i++){
+            console.log(objectArray[i]);
+        }
+    }
 });
 
